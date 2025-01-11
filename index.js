32e09758524f3bafbd024b68bf2f2b15544d4621
Vishwanth V <vishwanth.wise@gmail.com>
2025-01-11 13:38:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-let c = 0;
-document.getElementById("count").innerHTML = c;
+let c = 0
+document.getElementById("count").innerHTML = c
 
 document.getElementById("but").onclick = function() {
-    c = c + 1;
-    document.getElementById("count").innerHTML = c;
-};
+    c = c + 1
+    document.getElementById("count").innerHTML = c
+}
 
 document.getElementById("nut").onclick = function() {
-    c = c - 1;
-    document.getElementById("count").innerHTML = c;
-};
-
+    c = c - 1
+    document.getElementById("count").innerHTML = c
+}
 
 document.getElementById("resetInput").onchange = function() {
-    c = parseInt(document.getElementById("resetInput").value, 10);
-    document.getElementById("count").innerHTML = c;
-};
+    c = parseInt(document.getElementById("resetInput").value, 10)
+    document.getElementById("count").innerHTML = c
+}
 
+document.getElementById("colourmode").onclick = function() {
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+}
 
-
-
+document.getElementById("colourmode1").onclick = function() {
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+}       
